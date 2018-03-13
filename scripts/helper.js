089ae42b92bddc018c471cc910dcb44713b079c6
Hargobind Atwal <ethaipath@gmail.com>
2018-03-12 21:26:32 -0700

    script loader works!
    Now I just have to figure out a way to dynamically deal with deeper
    dirs.
    I imagine a recursive function is in order.

---
+++ scripts/helper.js
@@ -0,0 +1,40 @@
+function  facingAngle(startX, startY, targetX, targetY) {
+  return Math.atan((startY-targetY)/(startX-targetX)); 
+}
+const sayStuff = (thingsToBeSaid, whereToSayThem, colorToSayThem = '#000000', howBigToSayThem = '14') => {
+  // draw debug info
+  c.font = howBigToSayThem + 'px serif';
+  c.fillStyle = colorToSayThem;
+  thingsToBeSaid.forEach( (i, n) => {
+    // let iWidth = c.measureText(i).width;
+    c.fillText(i, whereToSayThem.x, whereToSayThem.y + (howBigToSayThem * n));
+  });
+}
+
+
+function moveOneRadTowardsTarget(startX, startY, targetX, targetY, radius) {
+    var angleRad = facingAngle(startX, startY, targetX, targetY);
+    let pos = {
+      x: radius * Math.cos(angleRad),
+      y: radius * Math.sin(angleRad)
+    }; 
+    if (startX > targetX && startY < targetY || startX > targetX && startY > targetY) {
+      pos.x = -pos.x;
+      pos.y = -pos.y;
+    }
+    return pos;
+}
+
+const detectCollison = (rect1, rect2, action) => {
+  if (rect1.x < rect2.x + rect2.size &&
+    rect1.x + rect1.size > rect2.x &&
+    rect1.y < rect2.y + rect2.size &&
+    rect1.size + rect1.y > rect2.y) {
+     // collision detected!
+     action(rect1, rect2);
+     return true;
+ }
+ return false;
+}
+
+
---
+++ scripts/scriptLoader.js
@@ -0,0 +1,20 @@
+const scriptNames = [
+    'helper.js',
+    'classes.js',
+    'setup.js'
+];
+
+
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script"); // Make a script DOM node
+    script.src = url; // Set it's src to the provided URL
+    script.type = 'text/javascript';
+
+    document.body.appendChild(script); // Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+    
+}
+
+const scrdir = 'scripts/';
+scriptNames.forEach( scr => {
+    dynamicallyLoadScript(scrdir + scr)
+});
