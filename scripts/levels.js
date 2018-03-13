559d05808055c31e65b5b3c2e24fadb81b0c9494
Hargobind Atwal <ethaipath@gmail.com>
2018-03-12 22:36:45 -0700

    added another level
    Something tells me I'll need to aproch this from a different angle.
    Maybe allow for more uniform level objects that have constructors that
    can be extended if need be.

--- scripts/levels.js
+++ scripts/levels.js
@@ -10,7 +10,7 @@ let levels = [
     }
   },
   {
-    goal: () => { return false; },
+    goal: () => score > 500,
     update: () => {
       if (!(count % 120) && rate !== 1) { rate--; }
       
@@ -21,19 +21,36 @@ let levels = [
         entities.push(new Zombie( 0, (innerHeight * Math.random())));
         entities.push(new Zombie( (innerWidth * Math.random()), 0));
       }
-      // if ( !(score % 100) ) { epoch++; }
+      if ( this.goal ) { epoch++; }
     }
+  },
+  {
+    hasRun: false,
+    init: () => {
+      // zombies get faster
+      let zombies = entities.filter( e => e.hasOwnProperty('zombie'));
+      zombies.forEach( e => e.d++ );
+    },
+    update: () => {
+      entities.push(new Zombie( (innerWidth * Math.random()), innerHeight));
+      // goal
+      // if (score > 100) { epoch++; }
+    }  
   }
 ]
 let currentLevel = 'some shit';
 let levelObj;
 const levelLoader = (level) => {
+  if (levels[level].hasOwnProperty('init') && 
+    currentLevel !== level) {
+    levels[level].init();
+    levels[level].hasRun = true;
+  }
   if (levels.length > level) {
     levels[level].update();
   } else {
     levels.push(level[0]);
-    // zombies get faster
-    let zombies = entities.filter( e => e.hasOwnProperty('zombie'));
-    zombies.forEach( e => e.d++ );
+
   }
+  currentLevel = level;
 }
